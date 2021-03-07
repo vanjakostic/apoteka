aa187b21fd9cf75172b8bf383ca6bf041d70f0e4
vanjakostic <62511596+vanjakostic@users.noreply.github.com>
2021-03-07 17:32:09 +0100

    Add files via upload

--- Proizvod.java
+++ Proizvod.java
@@ -1,8 +1,11 @@
 package apoteka;
 
+enum Proizvodjaci { COLGATE, SENSODYNE, NIVEA, MIXA, GARNIER, EUCERIN, MAYBELLINE, GOLDEN_ROSE, GALENIKA, HEMOFARM, FARMASWISS}
+
 public class Proizvod {
 	
 	protected String naziv;
+	protected Proizvodjaci proizvodjac;
 	protected String datumProizvodnje;
 	protected String rokTrajanja;
 	protected String mestoProizvodnje;
@@ -12,9 +15,10 @@ public class Proizvod {
 	
 	//konstruktor sa parametrima
 	
-	Proizvod(String naziv, String datumProizvodnje, String rokTrajanja,String mestoProizvodnje, double cena, boolean dostupan){
+	Proizvod(String naziv, Proizvodjaci proizvodjac, datumProizvodnje, String rokTrajanja,String mestoProizvodnje, double cena, boolean dostupan){
 		
 		this.naziv=naziv;
+		this.proizvodjac=proizvodjac;
 		this.datumProizvodnje=datumProizvodnje;
 		this.rokTrajanja=rokTrajanja;
 		this.mestoProizvodnje=mestoProizvodnje;
@@ -26,6 +30,7 @@ public class Proizvod {
 	//geteri
 	
 	String getNaziv() {return naziv; }
+	Proizvodjaci getProizvodjac() {	return proizvodjac; }
 	String getDatumProizvodnje() {return datumProizvodnje; }
 	String getRokTrajanja() {return rokTrajanja; }
 	String getMestoProizvodnje() {return mestoProizvodnje; }
@@ -37,13 +42,14 @@ public class Proizvod {
 	//{}
 	
 	void setNaziv(String naziv) {this.naziv=naziv; }
+	void setProizvodjac(Proizvodjaci proizvodjac){this.proizvodjac=proizvodjac; }
 	void setDatumProizvodnje(String datumProizvodnje) {this.datumProizvodnje=datumProizvodnje; }
 	void setRokTrajanja(String rokTrajanja) {this.rokTrajanja=rokTrajanja; }
 	void setMestoProizvodnje(String mestoProizvodnje) {this.mestoProizvodnje=mestoProizvodnje; }
 	void setCena(double cena){
 		
 		if(cena > 0)
-		this.cena=cena; 
+			this.cena=cena; 
 		
 	}
 	void setDostupan(boolean dostupan){this.dostupan=dostupan; }
