47baac1c4c3c7437d2d0ca2272400ddcde954682
vanjakostic <62511596+vanjakostic@users.noreply.github.com>
2021-03-11 17:40:14 +0100

    Add files via upload

--- Cetkice.java
+++ Cetkice.java
@@ -4,27 +4,27 @@ public class Cetkice extends Proizvod {
 	
 	enum Boje {PLAVA, ROZA, CRVENA, LJUBICASTA, ZUTA, ZELENA, BELA, NARANDZASTA, SIVA, CRNA}
 	
-	private Boje boja;
+	private Boje bojacetkice;
 	private boolean elektricna;
 	private int brojupakovanju;
 	
 	Cetkice(String naziv, Proizvodjaci proizvodjac, String datumProizvodnje, String rokTrajanja,
 			String mestoProizvodnje, double cena, boolean dostupan, int brojproizvoda,
-			Boje boja, boolean elektricna, int brojupakovanju){
+			Boje bojacetkice, boolean elektricna, int brojupakovanju){
 		
 		
 		super(naziv,proizvodjac,datumProizvodnje,rokTrajanja,mestoProizvodnje,cena,dostupan,brojproizvoda);
-		this.boja=boja;
+		this.bojacetkice=bojacetkice;
 		this.elektricna=elektricna;
 		this.brojupakovanju=brojupakovanju;
 		
 	}
 	
-	Boje getBoja() {return boja; }
+	Boje getBoja() {return bojacetkice; }
 	boolean getElektricna() {return elektricna; }
 	int getBrojupakovanju() {return brojupakovanju; }
 	
-	void setBoja(Boje boja) {this.boja=boja;}
+	void setBoja(Boje bojacetkice) {this.bojacetkice=bojacetkice;}
 	void setElektricna(boolean elektricna) {this.elektricna=elektricna;}
 	void setBrojupakovanju (int brojupakovanju) {this.brojupakovanju=brojupakovanju;}
 	
