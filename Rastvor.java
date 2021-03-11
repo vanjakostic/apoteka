5ad7eed4f33771ca644b9051fee918e66c1a6320
vanjakostic <62511596+vanjakostic@users.noreply.github.com>
2021-03-11 17:45:39 +0100

    Add files via upload

--- Rastvor.java
+++ Rastvor.java
@@ -1,12 +1,11 @@
 package apoteka;
 
-import apoteka.Cetkice.Boje;
 
 public class Rastvor extends Proizvod {
 	
-	enum Boje {MENTA, BUBBLEGUM, BRESKVA, VISNJA , LUBENICA, ZELENICAJ, JAGODA}
+	enum Ukus {MENTA, BUBBLEGUM, BRESKVA, VISNJA , LUBENICA, ZELENICAJ, JAGODA}
 	
-	private Boje bojarastvora;
+	private Ukus ukusrastvora;
 	private boolean izbeljuje;
 	private boolean gutanjeOpasno;
 	private boolean jacanjeDesni;
@@ -14,21 +13,21 @@ public class Rastvor extends Proizvod {
 	
 	public Rastvor (String naziv, Proizvodjaci proizvodjac, String datumProizvodnje, String rokTrajanja,
 			String mestoProizvodnje, double cena, boolean dostupan, int brojproizvoda,
-			Boje boja, boolean elektricna, int brojupakovanju) {
+			Ukus ukusrastvora, boolean elektricna, int brojupakovanju) {
 		
 		super(naziv,proizvodjac,datumProizvodnje,rokTrajanja,mestoProizvodnje,cena,dostupan,brojproizvoda);
-		this.bojarastvora=bojarastvora;
+		this.ukusrastvora=ukusrastvora;
 		this.izbeljuje=izbeljuje;
 		this.gutanjeOpasno=gutanjeOpasno;
 		this.jacanjeDesni=jacanjeDesni;
 	}
 	
-	Boje getBoju() {return bojarastvora; }
+	Ukus getUkus() {return ukusrastvora; }
 	boolean getIzbeljuje () {return izbeljuje;}
 	boolean getGutanjeOpasno () {return gutanjeOpasno;}
 	boolean getJacanjeDesni () {return jacanjeDesni;}
 	
-	void setBoju(Boje bojarastvora) { this.bojarastvora=bojarastvora; }
+	void setUkus(Ukus ukusrastvora) { this.ukusrastvora=ukusrastvora; }
 	void setIzbeljuje (boolean izbeljuje) {this.izbeljuje=izbeljuje; }
 	void setGutanjeOpasno (boolean gutanjeOpasno) {this.gutanjeOpasno=gutanjeOpasno; }
 	void setJacanjeDesni (boolean jacanjeDesni) {this.jacanjeDesni=jacanjeDesni; }
