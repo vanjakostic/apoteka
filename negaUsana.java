2f0ab0920f1281884d4f97411df27b39b21db2c9
teodoravranes <62737677+teodoravranes@users.noreply.github.com>
2021-05-11 00:37:27 +0200

    Update negaUsana.java

--- negaUsana.java
+++ negaUsana.java
@@ -1,32 +1,30 @@
-package apoteka;
+package Kozmetika;
+
+import apoteka.Proizvodjaci;
 
 enum negaUsanaNamena {RUZ, SJAJ, LABELO};
 
 class negaUsana extends kozmetika
 {
 negaUsanaNamena namena;
-boolean pomazePriIspucalosti;
 String boja;
 
 negaUsana(String naziv, Proizvodjaci proizvodjac, String datumProizvodnje, String rokTrajanja,
 		String mestoProizvodnje, double cena, boolean dostupan, int brojproizvoda,
 		boolean vegan, boolean prirodno, boolean organsko,
-		negaUsanaNamena namena, boolean pomazePriIspucalosti, String boja)
+		negaUsanaNamena namena, String boja)
 {
 	
 	super(naziv,proizvodjac,datumProizvodnje,rokTrajanja,mestoProizvodnje,cena,dostupan,brojproizvoda, vegan, prirodno, organsko);
 	this.namena = namena;
-	this.pomazePriIspucalosti = pomazePriIspucalosti;
 	this.boja = boja;
 
 }
 
 negaUsanaNamena getNamena() {return this.namena;}
-boolean getPomazePriIspucalosti() {return this.pomazePriIspucalosti;}
 String getBoja() {return this.boja;}
 
 void setNamena(negaUsanaNamena namena) {this.namena = namena;}
-void setPomazePriIspucalosti(boolean pomazePriIspucalosti) {this.pomazePriIspucalosti = pomazePriIspucalosti;}
 void setBoja(String boja) {this.boja = boja;}
 
 }
