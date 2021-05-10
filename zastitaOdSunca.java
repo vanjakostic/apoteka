8dc9ea65c84e7a4a2c28006f45bb14a8d7357cc4
teodoravranes <62737677+teodoravranes@users.noreply.github.com>
2021-05-11 00:39:23 +0200

    Update zastitaOdSunca.java

--- zastitaOdSunca.java
+++ zastitaOdSunca.java
@@ -1,4 +1,6 @@
-package apoteka;
+package Kozmetika;
+
+import apoteka.Proizvodjaci;
 
 enum zastitaOdSuncaOblik {LOSION, GEL, SPREJ, STIK}
 enum zastitaOdSuncaTip {MINERALNA, HEMIJSKA}
@@ -8,34 +10,27 @@ class zastitaOdSunca extends kozmetika
 int SPF;
 zastitaOdSuncaOblik oblik;
 zastitaOdSuncaTip tip;
-boolean cinkOksid;
-boolean titaniumDioksid;
+
 
 zastitaOdSunca(String naziv, Proizvodjaci proizvodjac, String datumProizvodnje, String rokTrajanja,
 		String mestoProizvodnje, double cena, boolean dostupan, int brojproizvoda,
 		boolean vegan, boolean prirodno, boolean organsko,
-        int SPF, zastitaOdSuncaOblik oblik, zastitaOdSuncaTip tip, boolean cinkOksid, boolean titaniumDioksid)
+        int SPF, zastitaOdSuncaOblik oblik, zastitaOdSuncaTip tip)
 {
 	
 	super(naziv,proizvodjac,datumProizvodnje,rokTrajanja,mestoProizvodnje,cena,dostupan,brojproizvoda, vegan, prirodno, organsko);
 	this.SPF = SPF;
 	this.oblik = oblik;
 	this.tip = tip;
-	this.cinkOksid = cinkOksid;
-	this.titaniumDioksid = titaniumDioksid;
 
 }
 
 int getSPF() {return this.SPF;}
 zastitaOdSuncaOblik getOblik() {return this.oblik;}
 zastitaOdSuncaTip getTip() {return this.tip;}
-boolean getCinkOksid() {return this.cinkOksid;}
-boolean getTitaniumDioksid() {return this.titaniumDioksid;}
 
 void setSPF(int SPF) {this.SPF = SPF;}
 void setOblik(zastitaOdSuncaOblik oblik) {this.oblik = oblik;}
 void setTip(zastitaOdSuncaTip tip) {this.tip = tip;}
-void setCinkOksid(boolean cinkOksid) {this.cinkOksid = cinkOksid;}
-void setTitaniumDioksid(boolean titaniumDioksid) {this.titaniumDioksid = titaniumDioksid;}
 
 }
